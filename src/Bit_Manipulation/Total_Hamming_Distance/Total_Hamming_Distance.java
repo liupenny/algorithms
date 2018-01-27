5b926e11b4fb14ee7d41bc48ac9214c81d990771
651023146@qq.com <651023146@qq.com>
2018-01-27 23:49:57 +0800

    1.27

--- src/Bit_Manipulation/Total_Hamming_Distance/Total_Hamming_Distance.java
+++ src/Bit_Manipulation/Total_Hamming_Distance/Total_Hamming_Distance.java
@@ -7,18 +7,24 @@ public class Total_Hamming_Distance {
     public int totalHammingDistance(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
 
-        int tmp = 0, res = 0;
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i+1; j < nums.length; j++)
-            {
-                tmp = nums[i] ^ nums[j];
-                res += Integer.bitCount(tmp);
+        int res , cnt = 0;
+        for (int i = 0; i < 32; i++)  //这里必须是从0开始，不能从1.因为是进位的数
+        {
+            res = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if((nums[j]&(1<<i))!=0)
+                {
+                    res++;
+                }
             }
-        return res;
+            cnt += res * (nums.length - res);
+        }
+        return cnt;
     }
 
     public static void main(String[] args) {
         Total_Hamming_Distance t = new Total_Hamming_Distance();
+        System.out.println(2&(1<<1));
         System.out.println(t.totalHammingDistance(new int[]{4, 14, 2}));
     }
 }
