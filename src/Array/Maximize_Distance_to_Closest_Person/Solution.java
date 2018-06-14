659926742133f04395d74caab23304babef7b05c
pennyliu <651023146@qq.com>
2018-06-14 09:25:23 +0800

    test for HttpRequestException

---
+++ src/Array/Maximize_Distance_to_Closest_Person/Solution.java
@@ -0,0 +1,15 @@
+package Array.Maximize_Distance_to_Closest_Person;
+
+/**
+ * Created by PennyLiu on 2018/6/14.
+ */
+
+public class Solution {
+    public int maxDistToClosest(int[] seats) {
+
+    }
+
+    public static void main(String[] args) {
+        Solution t = new Solution();
+    }
+}
