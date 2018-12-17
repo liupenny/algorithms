4abd9aafe84ff9325c9582ea3e9b602da5d0f18f
pennyliu <651023146@qq.com>
2018-12-17 23:24:12 +0800

    dp

---
+++ src/Dynamic_Programming/CherryPickup/Solution.java
@@ -0,0 +1,51 @@
+package Dynamic_Programming.CherryPickup;
+
+/**
+ * @author PennyLiu
+ * @version V1.0
+ * @Project: leetcode
+ * @Description: TODO
+ * @date 2018/12/17
+ */
+public class Solution {
+    public static int cherryPickup(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        // M是步数t的最大值
+        int N = grid.length, M = (N << 1) - 1;
+        int[][] dp = new int[N][N];
+        dp[0][0] = grid[0][0];
+        int tmp = 0;
+
+        for (int t = 1; t < N; t++) {
+            for (int r1 = 0; r1 < N; r1++) {
+                for (int r2 = 0; r2 < N; r2++) {
+                    int c1 = t - r1, c2 = t - r2;
+                    if (c1 < 0 || c1 >= N || c2 < 0 || c2 >= N) {
+                        dp[r1][r2] = -1;
+                        continue;
+                    }
+                    if (grid[r1][c1] == -1 || grid[r2][c2] == -1) {
+                        dp[r1][r2] = -1;
+                        continue;
+                    }
+                    if (r1 == r2) {
+                        tmp += grid[r1][c1];
+                    } else {
+                        tmp += (grid[r1][c1] + grid[r2][c2]);
+                    }
+                    dp[r1][r2] = Math.max(Math.max(dp[r1-1][r2],dp[r1][r2-1]), Math.max(dp[r1][r2],dp[r1-1][r2-1])) + tmp;
+                }
+            }
+        }
+        return Math.max(dp[N-1][N-1],0);
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{0, 1, -1},
+                        {1, 0, -1},
+                        {1, 1, 1}};
+        System.out.println(cherryPickup(grid));
+    }
+}
--- src/String/StringToInteger/Solution.java
+++ src/String/StringToInteger/Solution.java
@@ -10,29 +10,58 @@ import java.util.regex.Pattern;
  * @date 2018/12/13
  */
 public class Solution {
-//    public static int StrToInt(String str) {
-//
-//
-//    }
+    public static int StrToInt(String str) {
+        if (str == null || str.length() <= 0) {
+            return 0;
+        }
+        char[] a = str.toCharArray();
+        int sign = 1;
+        int beginPos = 0, ans = 0;
+        if (a[0] == '-' || a[0] == '+') {
+            sign = (a[0] == '-' ? -1 : 1);
+            beginPos++;
+        }
+        for (int i = beginPos; i < a.length; i++) {
+            if (a[i] < 48 || a[i] > 57) {
+                return 0;
+            }
+            if (ans > Integer.MAX_VALUE/10 || (ans == Integer.MIN_VALUE/10 && a[i] - '0' > 7)) {
+                return 0;
+            }
+            ans = ans * 10 + a[i] - '0';
+        }
+        return sign * ans;
+    }
 
-    public static boolean isLeagal(String str) {
-        String pattern = "^-?[1-9]//d*$";
-        boolean a = Pattern.matches(str, pattern);
-        return a;
-//        if (str == null || str.length() <= 0 || !Pattern.matches(str, pattern)) {
-//            return false;
-//        } else {
-//            return true;
-//        }
-//        } else {
-//            String min = String.valueOf(Integer.MIN_VALUE);
-//            String max = String.valueOf(Integer.MAX_VALUE);
-//
-//        }
+    public static int myAtoi(String str) {
+        if (str == null || str.length() <= 0) {
+            return 0;
+        }
+        char[] a = str.toCharArray();
+        int sign = 1, i = 0, ans = 0;
+        while (i < a.length && a[i] == ' ') {
+            i++;
+        }
+        if (i < a.length && (a[i] == '-' || a[i] == '+')) {
+            sign = (a[i] == '-' ? -1 : 1);
+            i++;
+        }
+        while (i < a.length && a[i] >= '0' && a[i] <= '9') {
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && a[i] - '0' > 7)) {
+                if (sign == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            ans = ans * 10 + a[i] - '0';
+            i++;
+        }
+        return sign * ans;
     }
 
     public static void main(String[] args) {
-        String a = "-1995";
-        System.out.println(isLeagal(a));
+        String a = "2147483648";
+        System.out.println(myAtoi(a));
     }
 }
